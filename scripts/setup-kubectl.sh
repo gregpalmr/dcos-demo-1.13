9db589b575a8c82a35770171ececfbb2d66b1d4b
Greg Palmer <greg.palmer@mesosphere.com>
2019-05-17 15:54:35 -0400

    Added output to show haproxy console URL

--- scripts/setup-kubectl.sh
+++ scripts/setup-kubectl.sh
@@ -81,9 +81,15 @@ echo " Getting IP Addresses of the DC/OS agents running the HAProxy services "
 echo "     NOTE: You must run ssh-add to add your DC/OS cluster's SSH key to your cache for this to work"
 echo
 
-HAPROXY1_PUB_IP=$(priv_ip=$(dcos task k8s-api-proxies_k8s-1 | grep -v HOST | awk '{print $2}') && dcos node ssh --option StrictHostKeyChecking=no --option LogLevel=quiet --master-proxy --private-ip=$priv_ip --user=centos "curl http://169.254.169.254/latest/meta-data/public-ipv4"); echo && echo "HAPROXY1_PUB_IP:   $HAPROXY1_PUB_IP"
+HAPROXY1_PUB_IP=$(priv_ip=$(dcos task k8s-api-proxies_k8s-1 | grep -v HOST | awk '{print $2}') && dcos node ssh --option StrictHostKeyChecking=no --option LogLevel=quiet --master-proxy --private-ip=$priv_ip --user=centos "curl http://169.254.169.254/latest/meta-data/public-ipv4");
 
-HAPROXY2_PUB_IP=$(priv_ip=$(dcos task k8s-api-proxies_k8s-2 | grep -v HOST | awk '{print $2}') && dcos node ssh --option StrictHostKeyChecking=no --option LogLevel=quiet --master-proxy --private-ip=$priv_ip --user=centos "curl http://169.254.169.254/latest/meta-data/public-ipv4"); echo && echo "HAPROXY2_PUB_IP:   $HAPROXY2_PUB_IP"
+HAPROXY2_PUB_IP=$(priv_ip=$(dcos task k8s-api-proxies_k8s-2 | grep -v HOST | awk '{print $2}') && dcos node ssh --option StrictHostKeyChecking=no --option LogLevel=quiet --master-proxy --private-ip=$priv_ip --user=centos "curl http://169.254.169.254/latest/meta-data/public-ipv4"); 
+
+echo
+echo " Kubernetes API Server Proxy 1: $HAPROXY1_PUB_IP  - http://${HAPROXY1_PUB_IP}:9091/haproxy?stats "
+echo
+echo " Kubernetes API Server Proxy 2: $HAPROXY2_PUB_IP  - http://${HAPROXY2_PUB_IP}:9092/haproxy?stats "
+echo
 
 # Setup kubectl to use the proxies to the api-servers
 echo
@@ -103,6 +109,24 @@ dcos kubernetes cluster kubeconfig \
     --context-name=k8s-1 \
     --cluster-name=k8s-1 \
     --apiserver-url=https://${HAPROXY1_PUB_IP}:6443
+
+echo
+echo " Starting Kubernetes Web Console proxy with command:"
+echo "   kubectl proxy > /dev/null &"
+
+# Kill any old "kubectl proxy" commands running
+killall kubectl > /dev/null 2>&1
+kubectl proxy > /dev/null &
+sleep 3
+open -a "Google Chrome" http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
+
+echo
+echo " Use the following kubeconfig tokens to access the K8s Web console:"
+echo
+echo
+cat ~/.kube/config | grep token
+echo
+echo
 
 echo
 echo " kubectl setup for 2 Kubernetes clusters. To switch contexts, use these commands:"
